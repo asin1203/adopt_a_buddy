c53ebd78c2d71b30d080085b43db3fcb4a98b006
Aastha Singh <53982436+asin1203@users.noreply.github.com>
2020-09-18 22:38:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+app = Flask(__name__)
+
+# from sklearn.externals import joblib
+model = pickle.load(open('model2.pkl', 'rb'))
+
+
+@app.route('/', methods=['GET', 'POST'])
+def pred():
+    if request.method == 'POST':
+        # try:
+            params = request.form
+
+            duration = int(params['duration'])
+            color = int(params['color'])
+            breed = float(params['breed'])
+            x1 = int(params['x1'])
+            x2 = int(params['x2'])
+            condition = int(params['condition'])
+            height = int(params['height'])
+            length = int(params['width'])
+
+            # length, height = 0, 0
+            fv = [condition, color, length, height,  x1, x2, breed, duration]
+            fv = np.array(fv).reshape((1, -1))
+            result = model.predict(fv)
+            return render_template('result.html', result=result)
+        # except:
+        #     return render_template('index.html')
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
